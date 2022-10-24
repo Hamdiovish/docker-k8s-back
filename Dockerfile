b2e1ca311038e41e2496542a4c89572c951a8643
Hamdi Ben Salah <hamdi@fingenesis.com>
2022-10-24 01:42:02 +0100

    integrate dapr kafka bindings

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM openjdk:8-jdk-alpine
+FROM openjdk:11-jre-slim
 MAINTAINER Fakher SOUAYEH
 COPY build/libs/rest-service-1.1.0-SNAPSHOT.jar demo-backend-1.1.0.jar
 ENTRYPOINT ["java","-jar","/demo-backend-1.1.0.jar"]
---
+++ src/main/java/com/example/restservice/dapr/SubscriberController.java
@@ -0,0 +1,19 @@
+package com.example.restservice.dapr;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@RestController
+public class SubscriberController {
+    private static final Logger log = LoggerFactory.getLogger(SubscriberController.class);
+    @PostMapping(path = "/sample")
+    public Mono<String> getCheckout(@RequestBody(required = false) byte[] body) {
+        return Mono.fromRunnable(() ->
+                log.info("Received Message: " + new String(body)));
+    }
+}
