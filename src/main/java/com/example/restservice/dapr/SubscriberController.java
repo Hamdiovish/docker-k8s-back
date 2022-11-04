24a3165292244ee0bbb47e8baf3d839c2ac15005
Hamdi Ben Salah <hamdi@fingenesis.com>
2022-11-04 06:59:44 +0100

    update dapr sub

--- src/main/java/com/example/restservice/dapr/SubscriberController.java
+++ src/main/java/com/example/restservice/dapr/SubscriberController.java
@@ -1,5 +1,12 @@
 package com.example.restservice.dapr;
 
+import com.example.restservice.heroes.controller.HeroesControllerV2;
+import com.example.restservice.heroes.mapper.HeroMapper;
+import com.example.restservice.heroes.model.Hero;
+import com.example.restservice.heroes.model.HeroEntity;
+import com.example.restservice.heroes.repository.HeroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,15 +17,27 @@ import org.slf4j.LoggerFactory;
 
 @RestController
 public class SubscriberController {
-    private static final Logger log = LoggerFactory.getLogger(SubscriberController.class);
+    private static final Logger logger = LoggerFactory.getLogger(SubscriberController.class);
+
+    @Autowired
+    private HeroRepository heroRepository;
+
+    @Autowired
+    private HeroMapper heroMapper;
+
     @PostMapping(path = "/sample")
     public Mono<String> getCheckout(@RequestBody(required = false) byte[] body) {
         return Mono.fromRunnable(() ->
-                log.info("Received Message: " + new String(body)));
+                logger.info("Received Message: " + new String(body)));
     }
     @PostMapping(path = "/sample-pubsub")
-    public Mono<String> getPubSubCheckout(@RequestBody(required = false) byte[] body) {
-        return Mono.fromRunnable(() ->
-                log.info("Received PubSub Message: " + new String(body)));
+    public ResponseEntity<Void> getPubSubCheckout(@RequestBody(required = false) byte[] body) {
+        HeroEntity newHero = new HeroEntity(new String(body));
+        if (newHero != null && !newHero.getName().isEmpty()) {
+            logger.info("Saving " + newHero.toString());
+            HeroEntity savedHero = heroRepository.save(new HeroEntity(newHero.getName()));
+            return ResponseEntity.ok().build();
+        }
+        return ResponseEntity.badRequest().build();
     }
 }
